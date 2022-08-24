0f17bb1e48ccdedd0ebd32a17add94088bf99fa5
vsantu <vsantu578@gmail.com>
2022-08-24 23:31:28 +0530

    first commit

---
+++ src/main/java/in/santosh/App.java
@@ -0,0 +1,13 @@
+package in.santosh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
